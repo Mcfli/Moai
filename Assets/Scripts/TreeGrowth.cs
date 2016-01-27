5b4652d5dce0584e29f58ce118a23a862652d307
Nathan Irwin <nki818@mac.com>
2016-01-27 15:10:00 -0800

    updating scale changes

---
+++ Assets/Scripts/TreeGrowth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeGrowth : MonoBehaviour {
+
+	public string buttonName;
+	public float rate;
+	private float growthRate;
+	private float currScaleX;
+	private float currScaleY;
+	private float currScaleZ;
+
+	private void Start(){
+		currScaleX = transform.localScale.x;
+		currScaleY = transform.localScale.y;
+		currScaleZ = transform.localScale.z;
+	}
+
+	private void Update(){
+		currScaleX = transform.localScale.x;
+		currScaleY = transform.localScale.y;
+		currScaleZ = transform.localScale.z;
+		if(Input.GetButton(buttonName)){
+			transform.localScale += new Vector3 (currScaleX + rate/1000000, currScaleY + rate/1000000, currScaleZ + rate/1000000);
+		}
+		else {
+			// do not change
+		}
+	}
+}
