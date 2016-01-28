d4a866b4c6b6e2e848c95c76470b304fd93c4666
Nathan Irwin <nki818@mac.com>
2016-01-27 19:33:13 -0800

    growing seems to be working
    Updated script to use vector3 lerp to increment scale and set max scale

--- Assets/Scripts/TreeGrowth.cs
+++ Assets/Scripts/TreeGrowth.cs
@@ -5,26 +5,21 @@ public class TreeGrowth : MonoBehaviour {
 
 	public string buttonName;
 	public float rate;
-	private float growthRate;
-	private float currScaleX;
-	private float currScaleY;
-	private float currScaleZ;
-
-	private void Start(){
-		currScaleX = transform.localScale.x;
-		currScaleY = transform.localScale.y;
-		currScaleZ = transform.localScale.z;
-	}
+    public float targetScale;
+    public float growSpeed;
+    private Vector3 v3Scale;
 
 	private void Update(){
-		currScaleX = transform.localScale.x;
-		currScaleY = transform.localScale.y;
-		currScaleZ = transform.localScale.z;
 		if(Input.GetButton(buttonName)){
-			transform.localScale += new Vector3 (currScaleX + rate/1000000, currScaleY + rate/1000000, currScaleZ + rate/1000000);
+            Grow();
 		}
 		else {
 			// do not change
 		}
 	}
+
+    private void Grow() {
+        v3Scale = new Vector3(targetScale, targetScale, targetScale);
+        transform.localScale = Vector3.Lerp(transform.localScale, v3Scale, Time.deltaTime * growSpeed);
+    }
 }
