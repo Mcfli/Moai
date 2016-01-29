317c24161255dd4987834893eba716faea6b7716
aurarose <rlima@ucsc.edu>
2016-01-29 15:40:11 -0800

    Update Colors+colliders

--- Assets/Scripts/GenerationManager.cs
+++ Assets/Scripts/GenerationManager.cs
@@ -97,7 +97,7 @@ public class GenerationManager : MonoBehaviour {
 
     void updateChunks()
     {
-        time += 0.05f;
+        time += 0.001f;
         for (int i = loaded_chunks.Count - 1; i >= 0; i--)
         {
             Vector2 this_chunk = loaded_chunks[i];
@@ -115,7 +115,28 @@ public class GenerationManager : MonoBehaviour {
                 verts[j] = new Vector3(x, amplitude * NoiseGen.genPerlin(xpos, ypos, time), y);
             }
             chunk.GetComponent<MeshFilter>().mesh.vertices = verts;
+            chunk.GetComponent<MeshCollider>().sharedMesh = chunk.GetComponent<MeshFilter>().mesh;
+
+            Color[] colors = new Color[verts.Length];
+            for (int c = 0; c < verts.Length; c += 3)
+            {
+                float height = (verts[c].y + verts[c + 1].y + verts[c + 2].y) / 3;
+
+                // colors[i] = environmentMapper.colorAtPos(xpos,vertices[c].y,ypos)
+                Color color;
+                if (height > 10)
+                    color = new Color(0.9f, 0.9f, 0.9f);
+                else if (height > -30)
+                    color = new Color(0.1f, 0.4f, 0.2f);
+                else
+                    color = new Color(0.7f, 0.7f, 0.3f);
+                colors[c] = color;
+                colors[c + 1] = color;
+                colors[c + 2] = color;
+            }
+            chunk.GetComponent<MeshFilter>().mesh.colors = colors;
         }
+        
 
     }
 
