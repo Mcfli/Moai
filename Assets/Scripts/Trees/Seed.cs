8e7c09717a0fb46ef0b483fc9727d049516923b2
aurarose <rlima@ucsc.edu>
2016-02-02 13:24:08 -0800

    Seeds
    Added seeds, and reorganized material folder structure

---
+++ Assets/Scripts/Trees/Seed.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seed : MonoBehaviour
+{
+
+    public GameObject tree_object;
+    public float cull_radius;        // How far this must be from other seeds and trees in order to grow.
+    public float life_length;        // How long before the seed sprouts into  a tree (or dies)
+
+    private LayerMask tree_mask;
+    private float spawned_time;     // Stores what the game time was when this was spawned. 
+
+    // Use this for initialization
+    void Start()
+    {
+        spawned_time = Time.time;
+        tree_mask = LayerMask.GetMask("Tree");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - spawned_time > life_length)
+        {
+            Collider[] close_trees = Physics.OverlapSphere(transform.position, cull_radius, tree_mask);
+            if(close_trees.Length < 1)
+            {
+                var RandomRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+                Instantiate(tree_object, transform.position, RandomRotation);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Trees/TreeGrowth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeGrowth : MonoBehaviour {
+
+    public float target_scale;
+    public float grow_speed;
+    public float grow_speed_variance;
+
+    void Start()
+    {
+        grow_speed += Random.value * 2 * grow_speed_variance - grow_speed_variance;
+    }
+
+	private void Update(){
+        Grow();
+	}
+
+    private void Grow() {
+        Vector3 v3Scale = new Vector3(target_scale, target_scale, target_scale);
+        transform.localScale = Vector3.Lerp(transform.localScale, v3Scale, Time.deltaTime * grow_speed);
+    }
+}
