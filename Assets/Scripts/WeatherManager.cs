c7b8a1f466dad447c19cca2b49836074180fc1cb
aurarose <rlima@ucsc.edu>
2016-02-03 11:28:12 -0800

    Init

---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WeatherManager : MonoBehaviour {
+    public string cur_weather;      // String holding the current weather state
+    List<GameObject> Clouds;        // holds all currently loaded clouds
+
+    private int target_clouds;
+
+    //List<ParticleSystem> Precipitation - holds all currently loaded particle systems
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cur_weather == "rainy")
+            doRain();
+        else if (cur_weather == "sunny")
+            doSunny();
+        else
+            doSnowy();
+    }
+
+    private void doRain()
+    {
+        target_clouds = 20;
+    }
+
+
+    private void doSunny()
+    {
+
+    }
+
+
+    private void doSnowy()
+    {
+
+    }
+
+	
+	
+}
