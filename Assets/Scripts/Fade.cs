cbcf1276cd462e8d2f0b72f30a0db3a2ef71016d
Nathan Irwin <nki818@mac.com>
2016-02-03 20:34:55 -0800

    added scene switching, need to fix fading

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour {
+
+    public ScreenFader fade;
+    private bool pressed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!pressed)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                pressed = true;
+                fade.fadeIn = !fade.fadeIn;
+                if (SceneManager.GetActiveScene().buildIndex == 0)
+                    SceneManager.LoadScene(1);
+                else if (SceneManager.GetActiveScene().buildIndex == 1)
+                    SceneManager.LoadScene(0);
+                pressed = false;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/GrowCube.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowCube : MonoBehaviour {
+    public Vector3 target_scale;
+    public float grow_speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Grow();
+        }
+	}
+
+    void Grow()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, target_scale, Time.deltaTime * grow_speed);
+    }
+}
