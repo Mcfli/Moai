f554e4af8f63ccb08a33fb0080739cb5d230cc5b
aurarose <rlima@ucsc.edu>
2016-02-18 18:42:19 -0800

    ryantest

---
+++ Assets/Scripts/ShrineGrid.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrineGrid : MonoBehaviour {
+
+    int imatesssttttttttt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Testsssssss() { }
+}
