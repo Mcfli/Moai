ffd293f225f519681e4ed934bf0350744d16eabd
Nathan Irwin <nki818@mac.com>
2016-02-18 19:37:42 -0800

    skeleton generator

--- Assets/Scripts/ShrineGrid.cs
+++ Assets/Scripts/ShrineGrid.cs
@@ -1,22 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ShrineGrid : MonoBehaviour {
 
     public bool debug;
     public bool isDone;
+    public float size;
+    public int resolution;
+
+    private Dictionary<Vector2, List<GameObject>> curState;
+    private Dictionary<Vector2, List<GameObject>> targetState;
+
+    private List<GameObject> validObjects;
 
 	// Use this for initialization
 	void Start () {
-	    
+        curState = new Dictionary<Vector2, List<GameObject>>();
+        targetState = new Dictionary<Vector2, List<GameObject>>();
+        validObjects = new List<GameObject>();
+        genTargetState();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (!isDone)
+        {
+            updateCurState();
+            checkDone();
+        }
 	}
 
     public void enablePlacementItem()
+    {
+
+    }
+
+    private Vector2 realToGrid(Vector3 pos)
+    {
+        return new Vector2();
+    }
+
+    private void updateCurState()
+    {
+
+    }
+
+    private void checkDone()
+    {
+
+    }
+
+    private void genTargetState()
     {
 
     }
