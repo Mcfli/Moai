670c0b3841629f01520179913d75cc3342e08d26
Nathan Irwin <nki818@mac.com>
2016-02-18 18:47:53 -0800

    update

--- Assets/Scripts/ShrineGrid.cs
+++ Assets/Scripts/ShrineGrid.cs
@@ -1,19 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
-public class ShrineGrid: MonoBehaviour {
-=======
-<<<<<<< HEAD
 public class ShrineGrid : MonoBehaviour {
 
     int imatesssttttttttt;
 	// Use this for initialization
-	void Start () {
-	
-=======
-public class NewBehaviourScript : MonoBehaviour {
->>>>>>> origin/PCG_Puzzles
 
     public bool debug;
     public bool isDone;
@@ -23,16 +14,13 @@ public class NewBehaviourScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	    
->>>>>>> origin/PCG_Puzzles
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-<<<<<<< HEAD
 
     void Testsssssss() { }
-=======
->>>>>>> origin/PCG_Puzzles
+
 }
