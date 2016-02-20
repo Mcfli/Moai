4d471a334d310ac4174f2209c968b317c7fb52ca
Nathan Irwin <nki818@mac.com>
2016-02-20 15:34:55 -0800

    checkDone, genTargetState done

--- Assets/Scripts/ShrineGrid.cs
+++ Assets/Scripts/ShrineGrid.cs
@@ -8,6 +8,7 @@ public class ShrineGrid : MonoBehaviour {
     public bool isDone;
     public float size;
     public int resolution;
+    public int maxSolItems; // max items for a solution
 
     private Dictionary<Vector2, List<GameObject>> curState;
     private Dictionary<Vector2, GameObject> targetState;
@@ -48,24 +49,44 @@ public class ShrineGrid : MonoBehaviour {
 
     private void checkDone()
     {
-		bool valid = false;
-		bool doneCheck = false;
-
+        bool found = false;
+        
 		GameObject tarObj;
-		foreach (var target in targetState.Keys) {
+		foreach (var target in targetState.Keys)
+        {
+            found = false;
 			tarObj = targetState[target];
-			foreach (GameObject curObj in curState[target]) {
-				if (curObj == tarObj) {
-					valid = true;
+			foreach (GameObject curObj in curState[target]) 
+            {
+                // check if this is the target object
+				if (curObj == tarObj) 
+                {
+                    found = true;
 				}
-
-			
 			}
+            // if did not find target item
+            if(found == false)
+            {
+                isDone = false;
+                return;
+            }
 		}
+        // looped through all and did not return false, so we found all of them
+        isDone = true;
+        return;
     }
 
+    // Uses a random Vector2 based on the resolution size and uses a random index based on the count in validObjects to populate targetState dictionary.
     private void genTargetState()
     {
-
+        int numItems = 0;
+        numItems = Random.Range(1, maxSolItems);
+        for (int i = 0; i < numItems; i++)
+        {
+            Vector2 place = new Vector2(Random.Range(0, resolution),Random.Range(0, resolution));
+            int index = Random.Range(0, validObjects.Count);
+            GameObject placeObj = validObjects[index];
+            targetState[place] = placeObj;
+        }
     }
 }
