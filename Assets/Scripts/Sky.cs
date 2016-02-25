54c312884a889e8bc9406efce8d66d7792794bb5
Brian Lin <bjlin@ucsc.edu>
2016-02-25 11:08:31 -0800

    revision5

--- Assets/Scripts/Sky.cs
+++ Assets/Scripts/Sky.cs
@@ -3,63 +3,76 @@ using System.Collections;
 
 public class Sky : MonoBehaviour {
 	//G stands for Goal
-	private float atmThickG;
-	private Color tintG;
-	private Color groundG;
-	private float expoG;
+	private Color color1G;
+	private Color color2G;
+	private Color color3G;
+	private float exponent1G;
+	private float exponent2G;
+	private float intensityG;
 	
 	//D stands for Delta
-	private float atmThickD;
-	private Color tintD;
-	private Color groundD;
-	private float expoD;
+	private Color color1D;
+	private Color color2D;
+	private Color color3D;
+	private float exponent1D;
+	private float exponent2D;
+	private float intensityD;
 	
 	private float timeRemain;
 
     void Start(){
-		changeSky(1.0f, new Color(0.5f, 0.5f, 0.5f, 1.0f), new Color(0.369f, 0.349f, 0.341f, 1.0f), 1.3f, 0); //default
-		//changeSky(1.0f, new Color(1.0f, 0.0f, 0.0f, 1.0f), new Color(0.369f, 0.349f, 0.341f, 1.0f), 1.3f, 10000); //red sky
+		changeSky(new Color(0.22f, 0.6f, 1.0f, 1.0f), new Color(0.8f, 0.592f, 0.961f, 1.0f), new Color(0.867f, 0.753f, 0.659f, 1.0f), 1.0f, 1.0f, 1.0f, 0); //default
     }
 
     void Update(){
 		if(timeRemain == 0) return;
 		
-		RenderSettings.skybox.SetFloat("_AtmosphereThickness", RenderSettings.skybox.GetFloat("_AtmosphereThickness") + atmThickD * Globals.time_scale);
-		RenderSettings.skybox.SetColor("_SkyTint", RenderSettings.skybox.GetColor("_SkyTint") + tintD * Globals.time_scale);
-		RenderSettings.skybox.SetColor("_GroundColor", RenderSettings.skybox.GetColor("_GroundColor") + groundD * Globals.time_scale);
-		RenderSettings.skybox.SetFloat("_Exposure", RenderSettings.skybox.GetFloat("_Exposure") + expoD * Globals.time_scale);
+		RenderSettings.skybox.SetColor("_Color1", RenderSettings.skybox.GetColor("_Color1") + color1D * Globals.time_scale);
+		RenderSettings.skybox.SetColor("_Color2", RenderSettings.skybox.GetColor("_Color2") + color2D * Globals.time_scale);
+		RenderSettings.skybox.SetColor("_Color3", RenderSettings.skybox.GetColor("_Color3") + color3D * Globals.time_scale);
+		RenderSettings.skybox.SetFloat("_Exponent1", RenderSettings.skybox.GetFloat("_Exponent1") + exponent1D * Globals.time_scale);
+		RenderSettings.skybox.SetFloat("_Exponent2", RenderSettings.skybox.GetFloat("_Exponent2") + exponent2D * Globals.time_scale);
+		RenderSettings.skybox.SetFloat("_Intensity", RenderSettings.skybox.GetFloat("_Intensity") + intensityD * Globals.time_scale);
 		timeRemain -=  Globals.time_scale;
 		
 		if(timeRemain <= 0){ //catch overshooting
-			RenderSettings.skybox.SetFloat("_AtmosphereThickness", atmThickG);
-			RenderSettings.skybox.SetColor("_SkyTint", tintG);
-			RenderSettings.skybox.SetColor("_GroundColor", groundG);
-			RenderSettings.skybox.SetFloat("_Exposure", expoG);
+			RenderSettings.skybox.SetColor("_Color1", color1G);
+			RenderSettings.skybox.SetColor("_Color2", color2G);
+			RenderSettings.skybox.SetColor("_Color3", color3G);
+			RenderSettings.skybox.SetFloat("_Exponent1", exponent1G);
+			RenderSettings.skybox.SetFloat("_Exponent2", exponent2G);
+			RenderSettings.skybox.SetFloat("_Intensity", intensityG);
 			timeRemain = 0;
 		}
     }
 	
-	//"time" is number of "time_resolution"s it takes to finish changing to new sky
+	//"time" is number of frames it takes to finish changing to new sky
 	//if time is 0, change sky immediately; time should not be less than 0
-	void changeSky(float atmThick, Color tint, Color ground, float expo, float time){
-		atmThickG = atmThick;
-		tintG = tint;
-		groundG = ground;
-		expoG = expo;
+	void changeSky(Color color1, Color color2, Color color3, float exponent1, float exponent2, float intensity, float time){
+		color1G = color1;
+		color2G = color2;
+		color3G = color3;
+		exponent1G = exponent1;
+		exponent2G = exponent2;
+		intensityG = intensity;
 		
 		if(time == 0){
-			RenderSettings.skybox.SetFloat("_AtmosphereThickness", atmThickG);
-			RenderSettings.skybox.SetColor("_SkyTint", tintG);
-			RenderSettings.skybox.SetColor("_GroundColor", groundG);
-			RenderSettings.skybox.SetFloat("_Exposure", expoG);
+			RenderSettings.skybox.SetColor("_Color1", color1G);
+			RenderSettings.skybox.SetColor("_Color2", color2G);
+			RenderSettings.skybox.SetColor("_Color3", color3G);
+			RenderSettings.skybox.SetFloat("_Exponent1", exponent1G);
+			RenderSettings.skybox.SetFloat("_Exponent2", exponent2G);
+			RenderSettings.skybox.SetFloat("_Intensity", intensityG);
 			timeRemain = time;
 			return;
 		}
 		
-		atmThickD = (atmThick - RenderSettings.skybox.GetFloat("_AtmosphereThickness"))/time;
-		tintD = (tint - RenderSettings.skybox.GetColor("_SkyTint"))/time;
-		groundD = (ground - RenderSettings.skybox.GetColor("_GroundColor"))/time;
-		expoD = (expo - RenderSettings.skybox.GetFloat("_Exposure"))/time;
+		color1D = (color1 - RenderSettings.skybox.GetColor("_Color1"))/time;
+		color2D = (color2 - RenderSettings.skybox.GetColor("_Color2"))/time;
+		color3D = (color3 - RenderSettings.skybox.GetColor("_Color3"))/time;
+		exponent1D = (exponent1 - RenderSettings.skybox.GetFloat("_Exponent1"))/time;
+		exponent2D = (exponent2 - RenderSettings.skybox.GetFloat("_Exponent2"))/time;
+		intensityD = (intensity - RenderSettings.skybox.GetFloat("_Intensity"))/time;
 		timeRemain = time;
 	}
 }
--- Assets/Scripts/SunScript.cs
+++ Assets/Scripts/SunScript.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 namespace UnityStandardAssets.Utility
 {
     public class SunScript : MonoBehaviour{
-        public float timeDivider = 100; //bigger it is, slower it moves - 100 is about 30min on Brian's computer
-		public float maxIntensity = 1;
-		public GameObject Moon; //moon should be attached to sun
+		public GameObject Player;
 		
-		private Vector3 originalPosition;
+        public float timeresPerDegree = 10;
+		//bigger it is, slower it moves
+		//360 degrees in a day, so
+		//1 is 1 degree per update or frame
+		//360 updates in a day
+		
+		public float maxIntensity = 1;
+		//public GameObject Moon; //moon should be attached to sun
+		
+		private Vector3 originalSunAngle;
 
         private void Start(){
-			originalPosition = transform.eulerAngles;
+			originalSunAngle = transform.eulerAngles;
         }
 
 
         // Update is called once per frame
         private void Update(){
-			Vector3 newRotation = new Vector3(Mathf.Repeat(Globals.time/Globals.time_resolution/timeDivider + originalPosition.x, 360), originalPosition.y, originalPosition.z);
+			Vector3 newRotation = new Vector3(Mathf.Repeat(Globals.time/Globals.time_resolution/timeresPerDegree + originalSunAngle.x, 360), originalSunAngle.y, originalSunAngle.z);
 			transform.eulerAngles = newRotation;
+			transform.position = new Vector3(Player.transform.position.x, 0, Player.transform.position.z);
 
             // Modulate intensity
             if (transform.eulerAngles.x < 180){
 				GetComponent<Light>().intensity = maxIntensity;
 				//GetComponent<Light>().intensity = (-Math.Abs(transform.eulerAngles.x - 90) / 90 + 1) * maxIntensity;
-				Moon.GetComponent<Light>().intensity = 0.0f;
+				//Moon.GetComponent<Light>().intensity = 0.0f;
 			}else{
 				GetComponent<Light>().intensity = 0.0f;
-				Moon.GetComponent<Light>().intensity = 0.5f;
+				//Moon.GetComponent<Light>().intensity = 0.5f;
 			}
         }
 
