aac60e20829bc5b70ab4c65ceb61a1ba214d3ac5
Brian Lin <bjlin@ucsc.edu>
2016-02-24 22:11:52 -0800

    revision4

--- Assets/Scripts/SunScript.cs
+++ Assets/Scripts/SunScript.cs
@@ -3,33 +3,32 @@ using UnityEngine;
 
 namespace UnityStandardAssets.Utility
 {
-    public class SunScript : MonoBehaviour
-    {
+    public class SunScript : MonoBehaviour{
+        public float timeDivider = 100; //bigger it is, slower it moves - 100 is about 30min on Brian's computer
+		public float maxIntensity = 1;
+		public GameObject Moon; //moon should be attached to sun
+		
+		private Vector3 originalPosition;
 
-        public float rot_speed;
-
-        public float maxIntensity;
-        //private float m_LastRealTime;
-        private Light sunLight;
-        private Vector3 rot_vector;
-
-        private void Start()
-        {
-            rot_vector = new Vector3(rot_speed, 0, 0);
-            //m_LastRealTime = Time.realtimeSinceStartup;
-            sunLight = GetComponent<Light>();
+        private void Start(){
+			originalPosition = transform.eulerAngles;
         }
 
 
         // Update is called once per frame
-        private void Update()
-        {
-            rot_vector = new Vector3(rot_speed*Globals.time_scale, 0, 0);
-            transform.Rotate(rot_vector * Time.deltaTime,Space.Self);
+        private void Update(){
+			Vector3 newRotation = new Vector3(Mathf.Repeat(Globals.time/Globals.time_resolution/timeDivider + originalPosition.x, 360), originalPosition.y, originalPosition.z);
+			transform.eulerAngles = newRotation;
 
             // Modulate intensity
-            if (transform.eulerAngles.x < 180) sunLight.intensity = (-Math.Abs(transform.eulerAngles.x - 90) / 90 + 1) * maxIntensity;
-            else sunLight.intensity = 0;
+            if (transform.eulerAngles.x < 180){
+				GetComponent<Light>().intensity = maxIntensity;
+				//GetComponent<Light>().intensity = (-Math.Abs(transform.eulerAngles.x - 90) / 90 + 1) * maxIntensity;
+				Moon.GetComponent<Light>().intensity = 0.0f;
+			}else{
+				GetComponent<Light>().intensity = 0.0f;
+				Moon.GetComponent<Light>().intensity = 0.5f;
+			}
         }
 
     }
