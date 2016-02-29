0913cf675f72e710b57bd688d898d74a66a789a5
Nathan Irwin <nki818@mac.com>
2016-02-29 14:12:56 -0800

    working on shrine placement

---
+++ Assets/Scripts/ShrineManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrineManager : MonoBehaviour {
+    public GenerationManager gen_manager;
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        gen_manager = gameObject.GetComponent<GenerationManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void placeShrine(int x, int y)
+    {
+        Vector3 position = new Vector3(x, 0, y);
+        Quaternion RandomRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+
+        GameObject new_shrine = Instantiate(prefab, position, RandomRotation) as GameObject;
+    }
+}
