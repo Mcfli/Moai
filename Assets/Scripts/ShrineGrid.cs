c341d288eb3f5c4a0078a7518e773e4371e2a8a6
aurarose <rlima@ucsc.edu>
2016-03-10 20:34:04 -0800

    Update the global heat-moisture vector
    To make future generated shrines use more biome puzzle elements

--- Assets/Scripts/ShrineGrid.cs
+++ Assets/Scripts/ShrineGrid.cs
@@ -343,10 +343,15 @@ public class ShrineGrid : MonoBehaviour {
 
     private void complete()
     {
+        // Make the shrine glow
         Instantiate(glow,transform.position+Vector3.up*10,Quaternion.identity); 
         GameObject glowInstance = Instantiate(glow,transform.position+Vector3.up*10,Quaternion.identity) as GameObject;
+        // Change the chunk
         GameObject.Find("WorldGen").GetComponent<GenerationManager>().modifyChunk(transform.position,heatMoistureChange);
+        // Add a star
         GameObject.Find("Sky").GetComponent<Sky>().addStar();
+        // Update puzzle complexity
+        Globals.heatMoistureVector += heatMoistureChange;
     }
 
     private Vector3 snapToTerrain(Vector3 pos)
