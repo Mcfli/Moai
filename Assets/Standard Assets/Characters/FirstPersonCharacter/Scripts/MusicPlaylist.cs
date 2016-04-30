71d947b6aa0db302ef2df22b5fabc35ef362b040
Enthrayu <atu4@ucsc.edu>
2016-04-29 17:45:48 -0700

    Completed
    Added in new script to add in new songs easily - Located in First Person
    Character
    
    -- Music Manager --
    
    -- Music Playlist --
    
    Still need to add in additional Mixer fixations for each effect

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MusicPlaylist.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlaylist : MonoBehaviour
+{
+    public bool ActivateOnAwake = true;
+    public AudioClip[] MusicList;
+
+    void Awake()
+    {
+        if (ActivateOnAwake && MusicManager.Instance)
+            MusicManager.Instance.ChangePlaylist(this);
+    }
+
+    void Start()
+    {
+        // Have playlist persist across scenes.
+        DontDestroyOnLoad(gameObject); // Don't destroy this object
+
+        // When a new scene is loaded, destroy the other playlists.
+        foreach (MusicPlaylist playlist in GameObject.FindObjectsOfType<MusicPlaylist>())
+        {
+            if (playlist.name != this.name)
+            {
+                Destroy(playlist.gameObject);
+            }
+        }
+    }
+
+}
