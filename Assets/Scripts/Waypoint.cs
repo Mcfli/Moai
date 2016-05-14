a3755a4fdc904a6ca44069b6be765269a6edbd7f
Nathan Irwin <nki818@mac.com>
2016-05-13 19:11:40 -0700

    working on waypoint

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+
+	// Use this for initialization
+	void OnEnable () {
+        transform.position = snapToTerrain(transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private Vector3 snapToTerrain(Vector3 pos)
+    {
+        Vector3 ret = pos;
+        RaycastHit hit;
+        Ray rayDown = new Ray(new Vector3(pos.x, 10000000, pos.z), Vector3.down);
+        int terrain = LayerMask.GetMask("Terrain");
+
+        if (Physics.Raycast(rayDown, out hit, Mathf.Infinity, terrain))
+        {
+
+            ret = new Vector3(pos.x, hit.point.y, pos.z);
+
+        }
+        return ret;
+    }
+}
