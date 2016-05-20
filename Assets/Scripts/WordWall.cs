3a57906223e4344d72c5b63693ef0b1e6c71d9c0
aurarose <rlima@ucsc.edu>
2016-05-20 09:17:44 -0700

    Initial commit

---
+++ Assets/Scripts/WordWall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WordWall : MonoBehaviour {
+
+    public List<WordWallText> texts;
+
+    private bool generated = false;
+    private Renderer rend;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!generated && Vector3.Distance(transform.position, Globals.Player.transform.position) < 100)
+        {
+            generateText();
+        }
+	}
+
+    private void generateText()
+    {
+        
+        generated = true;
+        
+    }
+}
