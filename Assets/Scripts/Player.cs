efe0650d9af0bfc6b4af28e66a4b4efa4e098a60
Nathan Irwin <nki818@mac.com>
2016-05-20 21:21:48 -0700

    Islandcamerawaiting (#124)
    * working on camera
    
    * fix camera issues on islands

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -86,7 +86,7 @@ public class Player : MonoBehaviour {
                     if(Physics.Raycast(rayDown, out hit, Mathf.Infinity, terrain)) {
                         camDistance += 5 * Time.deltaTime;
                         theta += 0.5f * Time.deltaTime;
-                        float targetY = hit.point.y - playerModel.transform.position.y + 80;
+                        float targetY = Mathf.Max(hit.point.y - playerModel.transform.position.y + 80, 20);
                         mainCamera.transform.localPosition = new Vector3(camDistance * Mathf.Cos(theta), Mathf.Lerp(mainCamera.transform.localPosition.y, targetY, Time.deltaTime), camDistance * Mathf.Sin(theta));
                     }
                 }
