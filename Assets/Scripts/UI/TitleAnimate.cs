49e2a8844390dcb8ef2592dddc5372e15007bf2e
Brian Lin <bjlin@ucsc.edu>
2016-05-27 15:34:36 -0700

    brb

---
+++ Assets/Scripts/UI/TitleAnimate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleAnimate : MonoBehaviour {
+    public float animationDuration; // seconds it takes to fade out
+    public Vector2 startingOffset;
+    private UnityEngine.UI.Image image;
+    private float enableTime = -1;
+
+    void Awake() {
+        image = GetComponent<UnityEngine.UI.Image>();
+    }
+
+    void Update() {
+        if(enableTime < 0) return;
+        if(Time.time - enableTime < animationDuration) {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, (Time.time - enableTime) / animationDuration);
+        } else {
+
+        }
+    }
+
+    public void animate() {
+        enableTime = Time.time;
+    }
+}
